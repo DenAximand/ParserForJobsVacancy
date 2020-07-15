7993365271dac7799ebd9f003d149d14d4a0095e
DenAximand <desperior@gmail.com>
2020-07-15 22:45:42 +0300

    second commit

--- Models/JobsList.php
+++ Models/JobsList.php
@@ -16,16 +16,22 @@ class JobsList
     public function go()
     {
         $html = file_get_html('https://jobs.dou.ua/vacancies/?city=%D0%94%D0%BD%D0%B5%D0%BF%D1%80&category=PHP&exp=0-1');
-        for($i=0;$i<=100;$i++)
+        if($html->innertext!='' and count($html->find('a[class=vt]')))
         {
-            $elementArr = $html->find('a[class=vt]',$i)->href;
-//            $type=gettype($elementArr);
-//            print_r($type);
-            if(array_key_exists($i,$html)){
-                break;
-            }
-            $this->vacancyLinks[]=$elementArr;
+           foreach ($html->find('a[class=vt]') as $item){
+               $this->vacancyLinks[] = $item->href;
+           }
         }
+//        for($i=0;$i<=100;$i++)
+//        {
+//            $elementArr = $html->find('a[class=vt]',$i)->href;
+//            if(array_key_exists($i,$html)){
+//                break;
+//            }
+//            $this->vacancyLinks[]=$elementArr;
+//        }
         print_r($this->vacancyLinks);
+        $html->clear();
+        unset($html);
     }
 }
